aa8298c7809e6ec61eaffb3d52cd1170f2b393c3
lincoln <linc.miller@gmail.com>
2019-08-28 22:57:36 -0400

    lesson08

--- app/src/main/java/com/ltmiller/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/ltmiller/coderswag/Controller/ProductsActivity.kt
@@ -1,17 +1,44 @@
 package com.ltmiller.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.widget.Adapter
+import android.widget.GridLayout
+import com.ltmiller.coderswag.Adapters.ProductsAdapter
 import com.ltmiller.coderswag.R
+import com.ltmiller.coderswag.Services.DataService
 import com.ltmiller.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+
+        if ( orientation == Configuration.ORIENTATION_LANDSCAPE ){
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenHeightDp
+
+        if ( screenSize > 720 ){
+            spanCount = 3
+        }
+
+        val layoutManger = GridLayoutManager(this, spanCount)
+
+        productsListView.layoutManager = layoutManger
+        productsListView.adapter = adapter
     }
 }
--- app/src/main/java/com/ltmiller/coderswag/Services/DataService.kt
+++ app/src/main/java/com/ltmiller/coderswag/Services/DataService.kt
@@ -7,11 +7,11 @@ object DataService {
 
     val categories = listOf(
         Category("SHIRTS", "shirtimage"),
-        Category("HODDIES", "hoodieimage"),
+        Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage"),
         Category("SHIRTS", "shirtimage"),
-        Category("HODDIES", "hoodieimage"),
+        Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
@@ -20,17 +20,45 @@ object DataService {
         Product("Devslopes Graphic Beanie", "$18", "hat1"),
         Product("Devslopses Hat Black", "$20", "hat2"),
         Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopses Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
+        Product("Devslopes Hat Snapback", "$22", "hat4"),
+        Product("Devslopes Graphic Beanie", "$18", "hat1"),
+        Product("Devslopses Hat Black", "$20", "hat2"),
+        Product("Devslopes Hat White", "$18", "hat3"),
         Product("Devslopes Hat Snapback", "$22", "hat4")
+
     )
 
     val hoodies = listOf(
         Product("Devslopes Hoodie Grey", "$28", "hoodie1"),
         Product("Devslopses Hoodie Red", "$32", "hoodie2"),
         Product("Devslopes Grey Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie4"),
+        Product("Devslopes Hoodie Grey", "$28", "hoodie1"),
+        Product("Devslopses Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Grey Hoodie", "$28", "hoodie3"),
+        Product("Devslopes Black Hoodie", "$32", "hoodie4"),
+        Product("Devslopes Hoodie Grey", "$28", "hoodie1"),
+        Product("Devslopses Hoodie Red", "$32", "hoodie2"),
+        Product("Devslopes Grey Hoodie", "$28", "hoodie3"),
         Product("Devslopes Black Hoodie", "$32", "hoodie4")
+
     )
 
     val shirts = listOf(
+        Product("Devslopes Shrit Black", "$18", "shirt1"),
+        Product("Devslopses Badge Light Grey", "$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devslopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "18", "shirt5"),
+        Product("Devslopes Shrit Black", "$18", "shirt1"),
+        Product("Devslopses Badge Light Grey", "$20", "shirt2"),
+        Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
+        Product("Devslopes Hustle", "$22", "shirt4"),
+        Product("Kickflip Studios", "18", "shirt5"),
         Product("Devslopes Shrit Black", "$18", "shirt1"),
         Product("Devslopses Badge Light Grey", "$20", "shirt2"),
         Product("Devslopes Logo Shirt Red", "$22", "shirt3"),
